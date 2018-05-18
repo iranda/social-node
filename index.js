59189e1d08a53011d59328436688130c1d313750
Daria Sirous <dsirous@tribalscale.com>
2018-05-18 14:43:08 -0400

    Adds middleware: body-parser, morgan

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(morgan('dev'));
+
 app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/all', (req, res) => {
+  res.send('sucess')
+});
+
 app.listen(3000);
